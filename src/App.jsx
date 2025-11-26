d7d37bea07f1f19a05fc55771db3d17a867a546f
wagnersjr <wagner.ramos2025@gmail.com>
2025-11-26 12:37:11 -0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Header from "./components/header/Header.jsx";
 import Main from "./components/main/Main.jsx";
 import Looks from "./components/looks/Looks.jsx";
-import Lancamentos from "./components/Lancamentos/Lancamentos.jsx";
+import Lancamentos from "./components/lancamentos/Lancamentos.jsx";
 
 function App() {
   return (
