c608e89ed233f962547b064b04f0d6d326bd4ded
wagnersjr <wagner.ramos2025@gmail.com>
2025-11-26 13:33:44 -0300

    consertando bugs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from "./components/Header/header.jsx";
 import Main from "./components/Main/main.jsx";
 import Looks from "./components/Looks/looks.jsx";
 import Lancamentos from "./components/Lancamentos/lancamentos.jsx";
-import Novidades from "./components/novidades/Novidades.jsx";
+import Novidades from "./components/Novidades/novidades.jsx";
 
 function App() {
   return (
